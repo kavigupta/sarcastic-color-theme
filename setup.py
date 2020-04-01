dbffd1ba87e7caf96aa1d577f931cc90d3fe4351
Kavi Gupta <kavi@berkeley.edu>
2020-03-31 19:10:54 -0700

    remove the archive?

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/kavigupta/sarcastic-color-scheme",
-    download_url="https://github.com/kavigupta/sarcastic-color-scheme/archive/0.1.0.zip",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.5",
